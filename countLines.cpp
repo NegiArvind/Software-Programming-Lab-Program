774243380f47f2fefc0c584523e021d63d64ecf4
NegiArvind <negiarvind229@gmail.com>
2019-01-17 14:49:13 +0530

    final program

--- countLines.cpp
+++ countLines.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-   int count=0;
+   int count=0,commentCount=0,countOpenBraces=0;
    string filename,line;
    cout<<"Enter filename\n";
    cin>>filename;
@@ -11,9 +11,24 @@ int main()
    while(getline(file,line))
    {
      cout<<line<<"\n";
-     count++;
-     cout<<count<<"\n";
+       for(int i=0;i<line.size();i++)
+       {
+          if(line[i]!=' ')
+          {
+		  if(line[i]=='{')
+		  countOpenBraces++;
+		  else if(line[i]=='/' && line[i+1]=='/')
+		  commentCount++;
+		  else
+		  count++;
+		  break;
+          }
+       }
+     //cout<<count<<"\n";
    }
-   cout<<"Number of lines ="<<count<<"\n";
+   cout<<"Number of open curly braces ="<<countOpenBraces<<"\n";
+   cout<<"Number of lines commented ="<<commentCount<<"\n";
+   cout<<"Number of lines un commented ="<<count<<"\n";
+   cout<<"total Number of lines ="<<2*countOpenBraces+commentCount+count<<"\n";
 }
 
